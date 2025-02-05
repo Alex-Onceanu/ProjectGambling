fd2b3f41c3a44b45e02456fc12fa4dda37aa6139
Alex Onceanu <aonceanu04@gmail.com>
2025-02-05 11:34:05 +0100

    Pop-up provisoire pour entrer le code de la partie

--- game_room.gd
+++ game_room.gd
@@ -1,16 +1,27 @@
 extends Node2D
 
+var user_id
+
 func _ready():
 	# on veut que la fonction _on_request_completed se lance une fois que la requête sera faite
 	$HTTPRequest.request_completed.connect(_on_request_completed)
-	
-	var code = "" # demander à l'utilisateur d'entrer le code de la game
-	var url = "https://" + code + ".ngrok-free.app"
-	$HTTPRequest.request(str(url) + "/register/user?name=leGodot")
-	
-	$Deck.deal_cards($Players/Player1, ["HA", "DA"], [$Players/Player6, $Players/Player4, $Players/Player7, $Players/Player2, $Players/Player5, $Players/Player3, $Players/Player8])
 
 # se lance dès que le serveur nous a répondu (la réponse est en argument)
 func _on_request_completed(result, response_code, headers, body):
 	var ans = body.get_string_from_utf8() # le serv répond juste un string
-	print(ans)
+	
+	# TODO : gestion d'erreurs (faire des try-catch)
+	user_id = int(ans)
+	print("ID : " + ans)
+	
+	$enter_code.visible = false
+	$Deck.deal_cards($Players/Player1, ["HA", "DA"], [$Players/Player6, $Players/Player4, $Players/Player7, $Players/Player2, $Players/Player5, $Players/Player3, $Players/Player8])
+
+# demander à l'utilisateur d'entrer le code de la game
+func _on_server_key_text_submitted(new_text: String) -> void:
+	if len($enter_code/server_key.text) <= 30:
+		return
+	var url = "https://" + $enter_code/server_key.text + ".ngrok-free.app"
+	$HTTPRequest.request(str(url) + "/register/user?name=leGodot")
+	
+	
