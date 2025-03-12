f70b8d8e59467d242045094d8442981ab6bc6778
Alex Onceanu <aonceanu04@gmail.com>
2025-03-12 10:39:44 +0100

    Les sacs d'argent deviennent + petits pour l'animation

--- scenes/money.gd
+++ scenes/money.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var initial_bag_pos
 
+@onready var initial_scale = $MoneyBag1.scale
 @onready var winners_pos = []
 @onready var nb_winners = 0
 
@@ -11,11 +12,13 @@ func set_winners(__winners):
 	$WinAnim.start()
 
 func easing(index, t):
-	var index01 = 0.8 * index / 10.0
+	var index01 = 0.5 * index / 10.0
 	if t <= index01:
 		return 0.0
-	var new_t = t - index01 / (1.0 - index01)
-	return new_t ** 0.35
+	elif t >= index01 + 0.5:
+		return 1.0
+	var new_t = 2.0 * (t - index01)
+	return new_t ** 0.46
 
 func _ready() -> void:
 	initial_bag_pos = []
@@ -27,11 +30,15 @@ func _process(delta: float) -> void:
 		var t = 1.0 - $WinAnim.time_left / $WinAnim.wait_time
 		for m in range(1, 11):
 			var eased_t = easing(m, t)
-			if get_node("MoneyBag" + str(m)).visible:
-				get_node("MoneyBag" + str(m)).global_position = lerp(initial_bag_pos[m - 1], winners_pos[m % nb_winners], eased_t)
+			var bag = get_node("MoneyBag" + str(m))
+			if bag.visible:
+				bag.scale = initial_scale.lerp(Vector2(0.1, 0.1), eased_t)
+				bag.global_position = lerp(initial_bag_pos[m - 1], winners_pos[m % nb_winners], eased_t)
 
 func _on_win_anim_timeout() -> void:
 	for m in range(1, 11):
-		if get_node("MoneyBag" + str(m)).visible:
-			get_node("MoneyBag" + str(m)).visible = false
-			get_node("MoneyBag" + str(m)).global_position = initial_bag_pos[m - 1]
+		var bag = get_node("MoneyBag" + str(m))
+		if bag.visible:
+			bag.visible = false
+			bag.scale = initial_scale
+			bag.global_position = initial_bag_pos[m - 1]
